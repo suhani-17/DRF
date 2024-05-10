cb54360753b691f08938a4c5d6696c3bfa435a4b
Suhani Jain <suhani.j2002@gmail.com>
2024-05-10 20:01:10 +0000

    Django rest APIS

---
+++ restTtutorial/snippets/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from snippets import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet, basename='snippet')
+router.register(r'users', views.UserViewSet, basename='user')
+
+urlpatterns = [
+     path('', include(router.urls)),
+]
+
